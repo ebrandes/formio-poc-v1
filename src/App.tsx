99cfb33a8c308a3b3c88df8a1382032bd9ba055e
eduardo.brandes <eduardo.brandes@ntconsultcorp.com>
2025-04-11 13:35:47 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "formiojs/dist/formio.full.min.css";
+import FormioForm from "./components/formio-form/formio-form";
+
+function App() {
+  return (
+    <>
+      <FormioForm />
+    </>
+  );
+}
+
+export default App;
